522c4dbe1c3c6af20f9fc41640d5ec1dd5d8bf5b
Rishabh <rishabhgoenka@gmail.com>
2024-06-06 20:28:39 +0100

    initial

---
+++ src/test/java/com/hsbc/myproj/MyprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.myproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
